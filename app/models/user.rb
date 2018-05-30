b95882a4216b537ae2af9f06bdb4b739d1633483
Sean <sean.n.cordes@gmail.com>
2018-05-30 18:27:20 -0400

    added length min 3 for username and unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: true
+  validates :username, presence: true,
+            length: { minimum: 3 }, uniqueness: true
 end
