8a7f3953a578642abc34625603ae102918f16902
Sean <sean.n.cordes@gmail.com>
2018-05-31 22:28:44 -0400

    reset post model/controller/views

---
+++ db/migrate/20180601000641_add_user_index_to_post.rb
@@ -0,0 +1,7 @@
+class AddUserIndexToPost < ActiveRecord::Migration[5.2]
+  def change
+    change_table :posts do |t|
+      t.references :user
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530234132) do
+ActiveRecord::Schema.define(version: 2018_06_01_000641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
